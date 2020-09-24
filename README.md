85303bc41bed0b8a8d3e34872d4f9809e7411b84
Sltang0815 <67080854+Sltang0815@users.noreply.github.com>
2020-09-23 20:21:39 -0700

    added to JS and described the assignment in the read me

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # weatherDashboard
+In this application I will be making a weather dashboard. 
+I will be making an HTML that has a side section that will be pulling from a weather API, a city of choice, from a form.
+
+The city that I will be searching will be displayed on the right side where it will preview a series of information. I will be pulling the tempurature, the name, wind speed, and the humidity.
+
+in the section of the city information, I will be creating a section with a premade div tag that will be blank as a container for the information to be placed in. For the UV index in need to have another Api link that will allow me to pull the uv index.
+
+in the section below I will be pulling a five day weathering api that will give me the information for the current 3 hours. It will be a separate api from the one day current city.
--- script.js
+++ script.js
@@ -1,31 +1,37 @@
 // weathering API
 let APIKey = "84f2dea59fdab1950d54a8cde450e6eb";
 
-//l URL where API is being pulled from
-let queryURL = "api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}" + APIKey;
+// URL where API is being pulled from
+// currentCity function has the parameter of city where it will pull from the URL and the response being what I am pulling and placing it in the $().html();
+function currentCity(city) {
+    let queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=84f2dea59fdab1950d54a8cde450e6eb&units=imperial";
 
-// AJAX call for API
-$.ajax({
-    url: queryURL,
-    method: "GET"
-})
+    // AJAX call for API
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    })
 
-    .then(function (response) {
+        .then(function (response) {
 
-        console.log(queryURL);
+            console.log(queryURL);
 
-        console.log(response);
+            console.log(response);
+            // let date vairable for date conversion slack
 
-        $(".city").html("<h1>", response.name);
-        $(".temp").text("Tempurature", response.);
-        $(".humidity").text("Humidity", response.);
-        $(".wind").text("Wind Speed", response.then);
-        $(".uvIndex").text("UV Index", response.);
+            $(".city").html("<h1>" + response.name + "</h1>");
+            $(".temp").text("Tempurature " + response.main.temp);
+            $(".humidity").text("Humidity " + response.main.humidity);
+            $(".wind").text("Wind Speed " + response.wind.speed);
+            // $(".uvIndex").text("UV Index", response.);
 
-    });
+            // API for the uv data    let queryURL= https://api.openweathermap.org/data/2.5/uvi?lat={lat}&lon={lon}&appid=&appid=84f2dea59fdab1950d54a8cde450e6eb&units=imperial";
 
+            // find out how to pull current city's long and lat  should bee in the "coord" of the console logged info
+        });
 
-then.preventDefault();
+}
+
 let searchBtn = document.querySelector(".searchBtn");
 let mainCity = document.querySelector(".currentInfo");
 let fourDays = document.querySelector(".fourDayInfo");
@@ -33,7 +39,53 @@ let fourDays = document.querySelector(".fourDayInfo");
 
 
 // onlcick for search button
-function(event) {
 
-    $(".searchBtn").on('click', function (event))
+$(".searchBtn").on('click', function (event) {
+    event.preventDefault();
+    let cityName = $("#cityLookUp").val().trim();
+    console.log(cityName);
+    currentCity(cityName);
+})
+
+
+// for the history under searche engine
+// localStorage.getItem();
+// localStorage.setItem();
+// JSON.stingify()      
+//.value() returns an array containing all the enumerable property values of the given object
+
+
+// addEventListener()  multiple click events
+
+
+// section to print out the F day line up of tempuratures. Appending to the HTML " id='fiveDay' " <div>
+//Figure out how to append
+
+
+function fiveDayLineUp(cities) {
+    let queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + cities + "&appid=84f2dea59fdab1950d54a8cde450e6eb&units=imperial";
+
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    })
+
+
+        .then(function (response) {
+
+            console.log(queryURL);
+
+            console.log(response);
+            // let date vairable for date conversion slack
+
+            // appending for a five day line up id "history"
+            $("#fiveDay").text.append(response);
+
+        });
+
+
+
+
+
 }
